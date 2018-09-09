48c9c1390a638f5d353eb7059c0bcaf2ac29de56
soroush <soroush.atarod@outlook.com>
2018-09-10 00:03:17 +0100

    add minimum_protocol_version

--- main.tf
+++ main.tf
@@ -4,94 +4,94 @@ locals {
 resource "aws_cloudfront_distribution" "cdn" {
 
     aliases = "${var.cnames}"
+    enabled = "${var.enabled}"
+    tags    = "${var.tags}"
+    
 
-        origin {
-            domain_name = "${var.domain_name}"
-            origin_id   = "${var.origin_id}"
+    origin {
+        domain_name = "${var.domain_name}"
+        origin_id   = "${var.origin_id}"
 
-            custom_origin_config {
-                http_port   = "${var.http_port}"
-                https_port  = "${var.https_port}"
-                origin_protocol_policy = "${var.origin_protocol_policy}"
-                origin_ssl_protocols = ["SSLv3","TLSv1"]
-            }
+        custom_origin_config {
+            http_port   = "${var.http_port}"
+            https_port  = "${var.https_port}"
+            origin_protocol_policy = "${var.origin_protocol_policy}"
+            origin_ssl_protocols = ["SSLv3","TLSv1"]
         }
+    }
 
-        enabled             = "${var.enabled}"
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
 
-        tags = "${var.tags}"
+    viewer_certificate {
+        cloudfront_default_certificate = false
+        acm_certificate_arn = "${var.acm_certificate_arn}"
+        ssl_support_method = "sni-only"
+        minimum_protocol_version = "TLSv1.1_2016"
+    }
 
-        default_cache_behavior {
+    default_cache_behavior {
+            allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+            cached_methods   = ["GET", "HEAD"]
+            target_origin_id = "${var.origin_id}"
 
-                allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-                cached_methods   = ["GET", "HEAD"]
-                target_origin_id = "${var.origin_id}"
+            forwarded_values {
+                query_string = true
+                headers = ["Host", "Origin"]
+                cookies {
+                    forward = "whitelist"
+                    whitelisted_names = "${var.cookies_whitelisted_names}"
+                }
+            }
 
-                forwarded_values {
-                    query_string = true
-                    headers = ["Host", "Origin"]
-                    cookies {
+            compress               = true
+            viewer_protocol_policy = "redirect-to-https"
+            min_ttl                = "${var.min_ttl}"
+            default_ttl            = "${var.default_ttl}"
+            max_ttl                = "${var.max_ttl}"
+    }
+
+
+        ordered_cache_behavior {
+            path_pattern     = "wp-admin/*"
+            allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+            cached_methods   = ["GET", "HEAD", "OPTIONS"]
+            target_origin_id = "${var.origin_id}"
+
+            forwarded_values {
+                query_string = true
+                headers = ["Host", "Origin"]
+                cookies {
                         forward = "whitelist"
                         whitelisted_names = "${var.cookies_whitelisted_names}"
-                    }
                 }
-
-                compress               = true
-                viewer_protocol_policy = "redirect-to-https"
-                min_ttl                = "${var.min_ttl}"
-                default_ttl            = "${var.default_ttl}"
-                max_ttl                = "${var.max_ttl}"
-
-        }
-
-
-         ordered_cache_behavior {
-                path_pattern     = "wp-admin/*"
-                allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-                cached_methods   = ["GET", "HEAD", "OPTIONS"]
-                target_origin_id = "${var.origin_id}"
-
-                forwarded_values {
-                    query_string = true
-                    headers = ["Host", "Origin"]
-                    cookies {
-                            forward = "whitelist"
-                            whitelisted_names = "${var.cookies_whitelisted_names}"
-                    }
-                }
-                compress               = true
-                viewer_protocol_policy = "redirect-to-https"
-         }
-
-         ordered_cache_behavior {
-                path_pattern     = "wp-login.php"
-                allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-                cached_methods   = ["GET", "HEAD", "OPTIONS"]
-                target_origin_id = "${var.origin_id}"
-
-                forwarded_values {
-                    query_string = true
-                    headers = ["Host", "Origin"]
-                    cookies {
-                            forward = "whitelist"
-                            whitelisted_names = "${var.cookies_whitelisted_names}"
-                    }
-                }
-
-                compress               = true
-                viewer_protocol_policy = "redirect-to-https"
-         }
-
-
-        restrictions {
-            geo_restriction {
-            restriction_type = "none"
             }
-        }
+            compress               = true
+            viewer_protocol_policy = "redirect-to-https"
+    }
 
-        viewer_certificate {
-            cloudfront_default_certificate = false
-            acm_certificate_arn = "${var.acm_certificate_arn}"
-            ssl_support_method = "sni-only"
-        }
+    ordered_cache_behavior {
+            path_pattern     = "wp-login.php"
+            allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+            cached_methods   = ["GET", "HEAD", "OPTIONS"]
+            target_origin_id = "${var.origin_id}"
+
+            forwarded_values {
+                query_string = true
+                headers = ["Host", "Origin"]
+                cookies {
+                        forward = "whitelist"
+                        whitelisted_names = "${var.cookies_whitelisted_names}"
+                }
+            }
+
+            compress               = true
+            viewer_protocol_policy = "redirect-to-https"
+    }
+
+
+        
 }
