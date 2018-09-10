cded21018a0554b6d35c3cfb3945b6883219f7aa
soroush <soroush.atarod@outlook.com>
2018-09-10 08:29:45 +0100

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "id" {
+  description = "The identifier for the distribution"
+  value       = "${aws_cloudfront_distribution.cdn.id}"
+}
+
+output "arn" {
+  description = "Amazon Resource Name for the distribution"
+  value       = "${aws_cloudfront_distribution.cdn.arn}"
+}
+
+output "hosted_zone_id" {
+  description = "The CloudFront Route 53 zone ID that can be used to route an Alias Resource Record Set"
+  value       = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+}
